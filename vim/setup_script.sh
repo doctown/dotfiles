220e962e94f254e0987715d50fc69a70f8472a5d
David Ogor <david.ogor@brightbytes.net>
2016-12-15 05:06:15 -0800

    Initial addition of Vundle and adding a script for additional files

---
+++ vim/setup_script.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Fetches all plugins and extras for my vim
+# Install vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+Ruby on Rails related Plugins
+# Install Vim goodies for Bundler
+git clone git://github.com/tpope/vim-bundler.git ~/.vim/bundle/vim-bundler
+
+# Rake.vim is a plugin leveraging projectionist.vim to enable you to use all those parts of rails.vim that you wish you could use on your other Ruby projects, including :A, :Elib and friends, and of course :Rake. It's great when paired with gem open and bundle open and complemented nicely by bundler.vim.
+
+git clone git://github.com/tpope/vim-rake.git
+git clone git://github.com/tpope/vim-projectionist.git
