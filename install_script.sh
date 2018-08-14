a2d0eb3a4fd639f91917b8e0b5b7e4ed4a098aca
David Ogor <dogor@twitter.com>
2018-08-14 11:42:15 -0700

    Attempt to use Prezto instead

--- install_script.sh
+++ install_script.sh
@@ -1,3 +1,10 @@
+# install ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# install Prezto
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+
 # Add all . files as ln in ~
 
 npm packages
@@ -12,3 +19,7 @@ sudo gem install neovim                       # Add ruby integration
 npm install -g neovim                        # add javascript integration
 
 pip3 install neovim-remote            # fixes for terminal neovim
+
+# Vim setup
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
